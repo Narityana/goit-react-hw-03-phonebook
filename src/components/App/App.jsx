a031178b558a00d8a319a5683323973d2cb2e24d
Тетяна Пономарчук <tanya061211@gmail.com>
2023-06-15 21:15:18 +0300

    done

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -27,7 +27,6 @@ export class App extends Component {
   }
 
   componentDidUpdate(prevProps, prevState) {
-    
     if (this.state.contacts !== prevState.contacts) {
       localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
     }
